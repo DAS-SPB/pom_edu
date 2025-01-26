493cf0464deb705dae8ce3e8a6aaa342cd01aa66
AntonioMont <garsiamarkes111@gmail.com>
2025-01-26 19:02:06 +0300

    "links manager" removed

--- config/links.py
+++
@@ -1,7 +0,0 @@
-class Links:
-
-    def __init__(self, host):
-        self.HOST = host
-        self.LOGIN_PAGE = f"{self.HOST}"
-        self.INVENTORY_PAGE = f"{self.HOST}/inventory.html"
-        self.CART_PAGE = f"{self.HOST}/cart.html"
