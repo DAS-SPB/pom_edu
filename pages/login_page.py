272f2733316d074d9a3aca466554a74b29ecb75d
AntonioMont <garsiamarkes111@gmail.com>
2025-01-31 23:06:15 +0300

    renamed 'enter_login' to 'enter_username' in login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -14,9 +14,9 @@ class LoginPage(BasePage):
         self.VALIDATION_ERROR_CONTAINER = ("xpath", "//div[contains(@class, 'error-message')]")
         self.VALIDATION_ERROR_TEXT = ("xpath", "//div[contains(@class, 'error-message')]/h3")
 
-    def enter_login(self, login):
-        with allure.step(f"Enter login: {login}"):
-            self.wait.until(EC.element_to_be_clickable(self.USERNAME_INPUT)).send_keys(login)
+    def enter_username(self, username):
+        with allure.step(f"Enter login: {username}"):
+            self.wait.until(EC.element_to_be_clickable(self.USERNAME_INPUT)).send_keys(username)
 
     def enter_password(self, password):
         with allure.step(f"Enter login: {password}"):
