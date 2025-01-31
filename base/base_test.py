c304af65d66982e43f91a69436d49bca5f7ab4a6
AntonioMont <garsiamarkes111@gmail.com>
2025-01-31 21:36:30 +0300

    BaseTest added to handle all pages in all test scenarios

---
+++ base/base_test.py
@@ -0,0 +1,28 @@
+import pytest
+from pages.login_page import LoginPage
+from pages.products_page import ProductsPage
+from pages.cart_page import CartPage
+from pages.checkout_step_one import CheckoutStepOnePage
+from pages.checkout_step_two import CheckoutStepTwoPage
+from pages.checkout_complete import CheckoutCompletePage
+
+
+class BaseTest:
+    login_page: LoginPage
+    products_page: ProductsPage
+    cart_page: CartPage
+    checkout_step_one: CheckoutStepOnePage
+    checkout_step_two: CheckoutStepTwoPage
+    checkout_complete: CheckoutCompletePage
+
+    @pytest.fixture(autouse=True)
+    def setup(self, request, driver, host):
+        request.cls.driver = driver
+        request.cls.host = host
+
+        request.cls.login_page = LoginPage(driver, host)
+        request.cls.products_page = ProductsPage(driver, host)
+        request.cls.cart_page = CartPage(driver, host)
+        request.cls.checkout_step_one = CheckoutStepOnePage(driver, host)
+        request.cls.checkout_step_two = CheckoutStepTwoPage(driver, host)
+        request.cls.checkout_complete = CheckoutCompletePage(driver, host)
