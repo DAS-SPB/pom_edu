248b6ff4fdd791e559faf09de602389f7dd14dd1
AntonioMont <garsiamarkes111@gmail.com>
2025-01-31 21:34:53 +0300

    updated work for use of PAGE_TITLE in is_opened

--- base/base_page.py
+++ base/base_page.py
@@ -8,6 +8,7 @@ class BasePage:
 
     def __init__(self, driver):
         self.PAGE_URL = None
+        self.PAGE_TITLE = None
         self.PAGE_NAME = self.__class__.__name__
         self.driver = driver
         self.wait = WebDriverWait(driver, 10, poll_frequency=1)
@@ -16,10 +17,10 @@ class BasePage:
         with allure.step(f"Open {self.PAGE_NAME} page"):
             self.driver.get(self.PAGE_URL)
 
-    def is_opened(self, page_title: str):
+    def is_opened(self):
         with allure.step(f"Page {self.PAGE_NAME} is opened"):
             self.wait.until(EC.url_to_be(self.PAGE_URL))
-            self.wait.until(EC.title_is(page_title))
+            self.wait.until(EC.title_is(self.PAGE_TITLE))
 
     def make_screenshot(self, screenshot_name):
         allure.attach(
